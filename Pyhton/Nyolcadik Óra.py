0e1b86984ede943dc40abcc5d53e05941b273493
Szszabi <67242037+szszabi2002@users.noreply.github.com>
2023-10-17 11:57:53 +0200

    második ora

--- Pyhton/Nyolcadik Óra.py
+++ Pyhton/Nyolcadik Óra.py
@@ -12,7 +12,7 @@ for i in range(1, a + 1):
     if a % i == 0:
         print(i)
 # Beolvasunk egy számot és kiírjuk hogy prim vagy összetet szám
-b = int(input("Adj meg egy számot:"))
+b = int(input("Adj meg egy számot és megmondom hogy prim e: "))
 
 
 if b > 2 and not any([True for x in range(2, b // 2 + 1) if b % x == 0]):
@@ -20,20 +20,57 @@ if b > 2 and not any([True for x in range(2, b // 2 + 1) if b % x == 0]):
 else:
     print(f"{b} összetett szám")
 
-if b > 2:
-    for i in range(2, (b % 2)+1):
-        if (b % i == 0):
-            print(f"{b} összetett szám")
-            break
-    else:
-        print(f"{b} prim szám")
-else:
-    print(f"{b} összetett szám")
+# if b > 2:
+#    for i in range(2, (b % 2)+1):
+#        if (b % i == 0):
+#            print(f"{b} összetett szám")
+#            break
+#    else:
+#        print(f"{b} prim szám")
+# else:
+#    print(f"{b} összetett szám")
+#
 db = 0
 for i in range(1, b + 1):
     if a % i == 0:
         db += 1
 if db == 2:
-    print('prim')
+    print("prim")
 else:
-    print('össetett')
+    print("össetett")
+# Beolvasunk egy számot és eldöntjük róla hogy tökéletes szám e?
+# A tökéletes számok osztóinak az összege a szám kétszeresével
+c = int(input("Adj meg egy számot és megmondom hogy tökéletes szám e: "))
+osztoksum = 0
+for i in range(1, c + 1):
+    if c % i == 0:
+        osztoksum += i
+if osztoksum == (c * 2):
+    print(c, "tökéletes szam")
+else:
+    print(c, "nem tökéletes szam")
+
+# Egymásba ágyazott ciklusok
+# ha két ciklus egymásba ágyazzunk az egyiket külsö ciklusnak a másikat pedig belső ciklusnak nevezzük
+# a külső ciklus minden egyes lépésnél a teljes a belső ciklusra lép
+# Kérjünk be egy számot és rajzoljunk ki egy csillagból egy ilyen oldalu négyzetet
+oldal = int(input("Adj egy számot 1 és 10 között: "))
+# Egymás mellé
+for i in range(oldal):
+    print("*")
+# Egymás allá
+for i in range(oldal):
+    print("*", end="")
+print("\n")
+for i in range(oldal):
+    print()
+    for j in range(oldal):
+        print("*\t", end="")
+print("\n")
+# Beolvasunk két számot és csinálunk *-ból téglalapot
+x = int(input("Adj egy számot 1 és 10 között: "))
+y = int(input("Adj egy számot 1 és 10 között: "))
+for i in range(x):
+    print()
+    for j in range(y):
+        print("*\t", end="")
