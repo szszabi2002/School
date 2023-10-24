c41794d6452c540b671fc6dc0547b8b858ffc0e0
Szszabi <67242037+szszabi2002@users.noreply.github.com>
2023-10-24 11:40:29 +0200

    Listák

--- Pyhton/Kilencedik Óra.py
+++ Pyhton/Kilencedik Óra.py
@@ -29,6 +29,24 @@ if (a >= 2):
                 is_Prime = False
                 break
         if (is_Prime):
-                print(i)
+            print(f"{i}, ", end="")
 else:
     print("A szám nem lehet kisebb 2-nél")
+print()
+# Listák összetet adatszerkezet, tratalmazhatja bármely típúst
+# általábban több elemet tartalmazhat egyszere ás ezekre az elemekre
+# INDEX haszálatával hívatkozhtunk
+# az index minding 0-val kezdődnek és [] -ben vannak
+# Üres lista  deklalárása: []
+# hivatkozás az lista első elemére lista[0]
+# hivatkozás az lista 10. elemére lista[9]
+# sztringek valójában karakterek listája hivatkozhatok index
+# akármelyik karakter
+
+nev = "Kiss Dezső"
+# Vegyük ki a névből az ő-betüt
+print(nev[9])
+print(nev[-1])
+
+# Írjuk ki q z betűt és az i betűt
+print(nev[-3], nev[1])
