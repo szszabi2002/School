77fd4b300a612658fa1987c39a03ae5958c3de20
Szszabi <67242037+szszabi2002@users.noreply.github.com>
2023-10-24 10:40:53 +0200

    init

---
+++ Pyhton/Kilencedik Óra.py
@@ -0,0 +1,8 @@
+##hozzuni létre egy 10-es szorzótáblát
+for i in range(0,11):
+        print(f"{i}x10=",i*10)
+#bekérünk egy számot és kiírjuk hogy prím-e
+
+
+
+#bekérünk egy számot és kiírjuk a prímeket addig a számig
