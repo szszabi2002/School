f2cfd225ed40f42396cd459b79f291ba71b01a9d
Szszabi <67242037+szszabi2002@users.noreply.github.com>
2023-11-07 11:59:16 +0100

    Második óra vége

--- Pyhton/Tizedik.py
+++ Pyhton/Tizedik.py
@@ -25,6 +25,46 @@ print(f"A keresztneved: {neve[index:]}")
 count = 0
 for j in range(len(neve)):
     if neve[j] == " ":
-        count+=1
-        break
+        count += 1
 print(f"A keresztnevek száma: {count}")
+# Beípitett sztringkezelő függvények metódusok
+# sztring hossza: LEN
+hossz = len(neve)
+# Tárolt sztring hosszát
+
+ujnev = neve.capitalize()
+# A .capitalize() a sztring legelső karakterét nagybetűvé alakítja
+
+# A .upper() a sztring minden karaktere nagy betű lesz
+ujnev1 = neve.upper()
+# A .lower() a sztring minden karaktere kisbetű lesz
+ujnev2 = neve.lower()
+# A .strip() eltávolítja a szóközöket a sztring elejtől a végéig
+ujnev3 = neve.strip()
+# A beírt sztrineket listává darabolja szét a megadott karakter mentén .split(Megadott karakter)
+ujnev4 = neve.split()
+lista = []
+lista = neve.split(" ")
+# Írjuk ki a lista elemeit
+for i in lista:
+    print(i)
+# A .find() keresi a karakter első előfordulásának indexét
+keres = neve.find('a')
+# A .startswith() igaz ha a sztring kezdődik 'a' karakternel
+kezdodo = neve.startswith('a')
+# A .endswith() igaz ha a sztring vége 'a' karakternel zárul
+vegulo = neve.endswith('a')
+
+# Bekérünk egy olyan mondatot és kiírjuk hogy hány karakterből áll
+mondat = input("Kérek egy mondatot: ")
+karakterek = len(mondat)
+print(f"A mondatban {karakterek} karakter van.")
+# hány szóból áll
+szavak = len(mondat.split())
+print(f"A mondatban {szavak} szót tartalmaz.")
+# mi a harmadik szó
+harmadik = mondat.split()[2]
+print(f"A harmadik szó a mondatba: {harmadik}")
+# mi a második szó 3. karaktere
+harmadikkarakter = mondat.split()[1][2]
+print(f"A második szó 3. karaktere: {harmadikkarakter}")
