51bf1703a5d881fb15ebfaa5d82ed3528b143b08
Szszabi <67242037+szszabi2002@users.noreply.github.com>
2023-11-13 08:30:19 +0100

    done

--- Webfejlesztés/Nyomtatás/nyomtatas.js
+++ Webfejlesztés/Nyomtatás/nyomtatas.js
@@ -1,12 +1,17 @@
-function kalkulal(){
+function kalkulal() {
     //Űrlapadatok
-    const szelesseg=297;
-    const magassag=420;
-    const papir=document.getElementById('papirtipus').value;
+    const szelesseg = document.getElementById('szelesseg').value;;
+    const magassag = document.getElementById('magassag').value;;
+    const papir = document.getElementById('papirtipus').value;
     //Számítások
-    let terulet=Math.round((szelesseg*magassag)/10000);    
-    let koltseg=terulet*papir;
+    let terulet = Math.round((szelesseg * magassag) / 10000);
+    let koltseg = terulet * papir;
+    let papirkoltseg = koltseg/terulet;
     //Megjelenítés
     document.getElementById('valasz').style.visibility = "visible";
+    document.getElementById("koltseg").innerHTML = koltseg;
+    document.getElementById("terulet").innerHTML = terulet;
+    document.getElementById("papir").innerHTML = papirkoltseg;
 }
+
 
