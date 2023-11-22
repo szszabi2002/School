e04c92d3117f515ecfa4706132aff67e8738d235
Szszabi <67242037+szszabi2002@users.noreply.github.com>
2023-11-22 13:05:45 +0100

    Óra vége

--- Webfejlesztés/Javascript Gyakorlás/script.js
+++ Webfejlesztés/Javascript Gyakorlás/script.js
@@ -18,11 +18,39 @@ JS alapok
             Tulajdonságai:
                 value
                 innerHTML
+    
+    Sztring típus:
+        'Ez egy szöveg'
+        "Ez egy szöveg"
+        ""Ez egy idézet"" Hibás
+        '"Ez egy idézet"'
+        "'Ez egy idézet'"
 
+        "It's a string"
+        'It\'s a string'
+        'It\\\'s a string'
+
+        "\"Ez egy idézet\""
+    
+    Változók hatóköre
+        for(let i = 0; i<5; i++){
+            console.log(i);
+        }
+
+        i = 12
+        for(let i = 0; i<5; i++){
+            console.log(i);
+        }
+        console.log(i)
+    Függvények paraméterei
+        Paraméter nélküli:
+            Pl.: function szamol(){}
+        Paramétres:
+            Pl.: function osszead(szamA, szamB){osszeg= szamA+szamB;return osszeg;}
+            osszead(2,3)
 */
 function szamitas() {
-    var szam1 = document.getElementById("szam1").value;
-    var szam2 = document.getElementById("szam2").value;
-    var eredmeny = szam1 * 1 + szam2 * 1;
-    document.getElementById("eredmeny").innerHTML = eredmeny;
+    var szam1 = Number(document.getElementById("szam1").value);
+    var szam2 = Number(document.getElementById("szam2").value);
+    document.getElementById("eredmeny").innerHTML = szam1 + szam2;
 }
