abc574e2d37f4e753c2e1a1666785afb81706805
Szszabi <67242037+szszabi2002@users.noreply.github.com>
2023-11-27 09:16:19 +0100

    Második Óra vége

--- Webfejlesztés/Űrlap feldolgozás/script.js
+++ Webfejlesztés/Űrlap feldolgozás/script.js
@@ -1,5 +1,47 @@
-function szamol() {
-    perc = document.getElementById("futasPerc").value;
-    perc = parseInt(perc);
-    document.getElementById("futasKal").value = (perc / 60 * 680).toFixed(2);
+function futasszamol() {
+    perc = document.getElementById('futasPerc').value;
+    perc = Number(perc);
+    //perc = parseInt(perc);
+    document.getElementById('futasKal').value = (perc / 60 * 680).toFixed(2);
+}
+function szamol(orankent, bePerc, eredmeny) {
+    perc = document.getElementById(bePerc).value;
+    perc = Number(perc);
+    if (isNaN(perc)) return perc = 0;
+    perOra = document.getElementById(orankent).innerHTML;
+    perOra = Number(perOra);
+    kiEredmeny = document.getElementById(eredmeny);
+    kiEredmeny.value = (perOra / 60 * perc).toFixed(2)
+    osszegez()
+}
+function osszegez(){
+    osszPerc = 0;
+    osszPerc +=  Number(document.getElementById('futasPerc').value)
+    osszPerc +=  Number(document.getElementById('fociPerc').value)
+    osszPerc +=  Number(document.getElementById('bringazasPerc').value)
+    if (isNaN(perc)) return perc = 0;
+    osszPerc += futasPerc;
+    futasPerc = document.getElementById('hegymaszasPerc').value
+    if (isNaN(perc)) return perc = 0;
+    osszPerc += futasPerc;
+    futasPerc = document.getElementById('lovaglasPerc').value
+    if (isNaN(perc)) return perc = 0;
+    osszPerc += futasPerc;
+    futasPerc = document.getElementById('turaPerc').value
+    if (isNaN(perc)) return perc = 0;
+    osszPerc += futasPerc;
+    futasPerc = document.getElementById('kajakPerc').value
+    if (isNaN(perc)) return perc = 0;
+    osszPerc += futasPerc;
+    futasPerc = document.getElementById('sulyedzPerc').value
+    if (isNaN(perc)) return perc = 0;
+    osszPerc += futasPerc;
+    futasPerc = document.getElementById('pingpongPerc').value
+    if (isNaN(perc)) return perc = 0;
+    osszPerc += futasPerc;
+    futasPerc = document.getElementById('kutyasetaPerc').value
+    if (isNaN(perc)) return perc = 0;
+    osszPerc += futasPerc;
+
+    document.getElementById('osszPerc').value = osszPerc;
 }
