6f637ee5e49cd497a2bc9e6c02233dc7cafd60d5
Szszabi <67242037+szszabi2002@users.noreply.github.com>
2024-01-02 11:31:28 +0100

    fix

--- Pyhton/ágazati1/Forrás/B - Programozás Pythonban/lootbox1.py
+++ Pyhton/ágazati1/Forrás/B - Programozás Pythonban/lootbox1.py
@@ -2,14 +2,11 @@ import random
 
 n = int(input("Mennyi húzás legyen: "))
 szamok = []
-osszeg = 0
 print("A húzott számok: ")
 for i in range(n):
     szam = random.randint(1, 40)
     szamok.append(szam)
-    osszeg += szam
     print(szam)
-szamok.sort()
 print(f"Legendary tárgyak: ", end="")
 sorsz = 0
 for i in szamok:
@@ -17,11 +14,11 @@ for i in szamok:
         print(f"{sorsz}:{i}", end=" ")
     sorsz += 1
 print()
-print(f"Rare tárgyak: ", end=" ")
+print(f"Rare tárgyak: ", end="")
 sorsz = 0
 for i in szamok:
     if i % 5 != 0 and i % 2 != 0:
-        print(f"{sorsz}:{i}", end="")
+        print(f"{sorsz}:{i}", end=" ")
     sorsz += 1
 print()
 print("A húzások összege:", sum(szamok))
