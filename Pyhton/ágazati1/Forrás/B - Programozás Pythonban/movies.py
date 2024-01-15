8eb45ba1c536fe488f25217557b2899e9b8b10ed
Szszabi <67242037+szszabi2002@users.noreply.github.com>
2024-01-15 13:27:10 +0100

    teszt

--- Pyhton/ágazati1/Forrás/B - Programozás Pythonban/movies.py
+++ Pyhton/ágazati1/Forrás/B - Programozás Pythonban/movies.py
@@ -18,7 +18,7 @@ def dollarToEuro(arfolyam, ertek):
 
 
 with open(
-    "C:/Users/szsza/OneDrive/School/School/Pyhton/ágazati1/Forrás/B - Programozás Pythonban/movies.txt",
+    "movies.txt",
     "r",
     encoding="utf8",
 ) as in_file:
