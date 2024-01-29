0ccefbd8b44a7d40ddf1d8cfd51ac46913e07f96
Szszabi <67242037+szszabi2002@users.noreply.github.com>
2024-01-29 08:35:54 +0100

    4.feladat

--- Adatbázis/Hajomenetrend/megoldas_hajo.sql
+++ Adatbázis/Hajomenetrend/megoldas_hajo.sql
@@ -3,3 +3,20 @@ CREATE DATABASE hajo_SzSz
 DEFAULT CHARACTER "utf8_hungarian_ci"
 
 /*2*/
+ALTER TABLE menetrend
+ADD COLUMN azon INT AUTO_INCREMENT PRIMARY KEY FIRST;
+
+/*3*/
+SELECT honnan, indul, hova, erkezik FROM menetrend
+WHERE jarat = 'J1'
+
+/*4*/
+SELECT DISTINCT `hova` FROM `menetrend` 
+WHERE `honnan` = 'Balatonfüred'
+AND `indul` >= '11:30' 
+AND `indul` <= '12:30'
+
+/*4.a*/
+SELECT DISTINCT `hova` FROM `menetrend` 
+WHERE `honnan` = 'Balatonfüred'
+AND `indul` BETWEEN '11:30' AND '12:30'
