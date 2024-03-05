0bc7f6ce735f8a115316caa5a88f32d613e65c21
Szszabi <67242037+szszabi2002@users.noreply.github.com>
2024-03-05 10:37:39 +0100

    init

---
+++ Adatbázis/Forma-1/megoldasok.sql
@@ -0,0 +1,40 @@
+﻿-- Active: 1706598324977@@127.0.0.1@3306@forma1_13i_gy2
+/*A feladatok megoldására elkészített SQL parancsokat illessze be a feladat sorszáma után!*/
+/*1. feladat:*/
+CREATE DATABASE forma1_13I_gy2 DEFAULT CHARACTER SET utf8 COLLATE utf8_hungarian_ci;
+
+
+/*3. feladat:*/
+CREATE INDEX pilotaIndex
+ON pilotak(pnev)
+
+/*4. feladat:*/
+ALTER TABLE pilotak
+ADD FOREIGN KEY (csapat)
+REFERENCES csapatok(csazon);
+
+ALTER TABLE eredmenyek
+ADD FOREIGN KEY (pilota)
+REFERENCES pilotak(pazon)
+ADD FOREIGN KEY (nagydij)
+REFERENCES versenyek(vkod)
+
+/*5. feladat:*/
+DELETE FROM versenyek
+WHERE vnev = 'Német Nagydíj'
+
+/*6. feladat:*/
+UPDATE versenyek SET hely = 'Sao Paulo'
+WHERE vnev = 'Brazil Nagydíj'
+
+/*7. feladat:*/
+SELECT pnev, COUNT(*) AS 'gyozelmek'
+FROM pilotak INNER JOIN eredmenyek ON pilotak.pazon = eredmenyek.pilota
+WHERE celpoz = 1
+GROUP BY pnev
+
+/*8. feladat:*/
+
+
+/*9. feladat:*/
+
