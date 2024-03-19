01cbf1b7e36f84dcb597e8ef9480c7b785f941d2
Szszabi <67242037+szszabi2002@users.noreply.github.com>
2024-03-19 14:23:55 +0100

    actual fix

--- Adatszerkezett/minrendezes.py
+++ Adatszerkezett/minrendezes.py
@@ -3,11 +3,11 @@ import random
 szamok = []
 
 for i in range(5):
-    szam = random.randint(1, 100000)
+    szam = random.randint(1, 20)
     szamok.append(szam)
 print(szamok)
 for i in range(len(szamok) - 1):
-    minimum = 1
+    minimum = i
     for j in range(i + 1, len(szamok)):
         if szamok[minimum] > szamok[j]:
             minimum = j
