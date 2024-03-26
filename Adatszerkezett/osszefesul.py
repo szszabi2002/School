469dd2d60467da769834c6f442371e946548033b
Szszabi <67242037+szszabi2002@users.noreply.github.com>
2024-03-26 13:36:54 +0100

    init

--- Adatszerkezett/osszefesul.py
+++ Adatszerkezett/osszefesul.py
@@ -1,5 +1,6 @@
 import random
 
+
 def mergeSort(myList):
     if len(myList) > 1:
         mid = len(myList) // 2
@@ -13,37 +14,38 @@ def mergeSort(myList):
         # A két félre létrehozunk két ciklusváltozót
         i = 0
         j = 0
-        
+
         # A fő lista ciklusváltozója
         k = 0
-        
+
         while i < len(left) and j < len(right):
             if left[i] <= right[j]:
-              # A bal oldali félből tesszük a listába az értéket
-              myList[k] = left[i]              
-              i += 1
+                # A bal oldali félből tesszük a listába az értéket
+                myList[k] = left[i]
+                i += 1
             else:
                 myList[k] = right[j]
                 j += 1
-            
+
             k += 1
 
-       # a kimaradt elemeket hozzáadjuk a listához
+        # a kimaradt elemeket hozzáadjuk a listához
         while i < len(left):
             myList[k] = left[i]
             i += 1
             k += 1
 
         while j < len(right):
-            myList[k]=right[j]
+            myList[k] = right[j]
             j += 1
             k += 1
 
+
 sz = []
 for i in range(5):
-    x = random.randint(1,100)
+    x = random.randint(1, 100)
     sz.append(x)
 print(sz)
-
+print()
 mergeSort(sz)
 print(sz)
