4ec7f78adcb9e4e9aa4f1d69e50a6d4905d92569
Szszabi <67242037+szszabi2002@users.noreply.github.com>
2024-04-09 13:26:48 +0200

    init

--- Adatszerkezett/rendezés.py
+++ Adatszerkezett/rendezés.py
@@ -2,7 +2,22 @@ import random
 
 szamok = []
 
-for i in range(5):
+for i in range(15):
+    szam = random.randint(1, 40)
+    szamok.append(szam)
+    print(szam, end=" ")
+print("")
+for i in range(0, len(szamok)):
+    for j in range(i + 1, len(szamok)):
+        if szamok[i] > szamok[j]:
+            temp = szamok[i]
+            szamok[i] = szamok[j]
+            szamok[j] = temp
+print(szamok)
+
+szamok = []
+
+for i in range(15):
     szam = random.randint(1, 40)
     szamok.append(szam)
     print(szam, end=" ")
