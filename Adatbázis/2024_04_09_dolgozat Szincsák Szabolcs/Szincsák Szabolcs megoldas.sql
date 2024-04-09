e2ed6cae50e0798b60f0276919339c19322c6286
Szszabi <67242037+szszabi2002@users.noreply.github.com>
2024-04-09 08:55:04 +0200

    init

---
+++ Adatbázis/2024_04_09_dolgozat Szincsák Szabolcs/Szincsák Szabolcs megoldas.sql
@@ -0,0 +1,31 @@
+/*1. Hány olyan felhasználó van az adatbázisban, akinek a keresztneve ’B’ betűvel kezdődik?*/
+
+
+/*2. Melyik a legdrágább rendelhető étel?*/
+
+
+/*3. Ki(k) rendeltek abból az ételből, amiből a legkevesebb fogyott?*/
+
+
+/*4. Melyik ételből volt a legtöbb bevétele és mennyi a hamburgerezőnek?*/
+
+
+/*5. Melyek   azok   a   felhasználók   akik   összesen   többet   költöttek   30.000Ft-nál   a   rendeléseik
+
+
+/*során?*/
+
+
+/*6. Melyik ételkategória ’Katona Vince’ kedvence?*/
+
+
+/*7. A nap melyik órájában rendelték a legtöbb desszertet?*/
+
+
+/*8. Ki rendelte a legtöbb hamburgert?*/
+
+
+/*9. Melyik kategóriához tartozik a legtöbb étel?*/
+
+
+/*10. Mely(ek) az(ok) a felhasználó(k) aki(k) a legtöbb ételkategóriából rendeltek?*/
