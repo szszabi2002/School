84bb4b928de732bfaf59f4622bc19fb4abd204fe
Szszabi <67242037+szszabi2002@users.noreply.github.com>
2024-06-04 10:38:31 +0200

    kiralyok majdnem kész

--- Adatbázis/Szincsák Szabolcs_ujABK/Megoldások/Szincsák Szabolcs_kiralyok.sql
+++ Adatbázis/Szincsák Szabolcs_ujABK/Megoldások/Szincsák Szabolcs_kiralyok.sql
@@ -1,49 +1,52 @@
 /*2*/
-SELECT
-  uralkodo.nev,
-  uralkodo.ragnev,
-  uralkodo.szul
+SELECT uralkodo.nev, uralkodo.ragnev, uralkodo.szul
 FROM kiralyok.uralkodo
 ORDER BY uralkodo.szul
 
-
 /*3*/
-SELECT
-  uralkodo.nev,
-  hivatal.mettol,
-  hivatal.meddig
+SELECT uralkodo.nev, hivatal.mettol, hivatal.meddig
 FROM kiralyok.uralkodo
-  INNER JOIN kiralyok.uralkodohaz
-    ON uralkodo.uhaz_az = uralkodohaz.azon
-  INNER JOIN kiralyok.hivatal
-    ON hivatal.uralkodo_az = uralkodo.azon
-WHERE uralkodohaz.nev LIKE "Árpád-ház"
+    INNER JOIN kiralyok.uralkodohaz ON uralkodo.uhaz_az = uralkodohaz.azon
+    INNER JOIN kiralyok.hivatal ON hivatal.uralkodo_az = uralkodo.azon
+WHERE
+    uralkodohaz.nev LIKE "Árpád-ház"
 ORDER BY 2
 
-
 /*4*/
+SELECT uralkodo.nev
+FROM kiralyok.uralkodo
+    INNER JOIN kiralyok.hivatal ON hivatal.uralkodo_az = uralkodo.azon
+WHERE
+    hivatal.koronazas < hivatal.mettol
+
+/*5*/
+SELECT COUNT(*)
+FROM kiralyok.uralkodo
+    INNER JOIN kiralyok.hivatal ON hivatal.uralkodo_az = uralkodo.azon
+WHERE
+    hivatal.mettol >= 1601
+    AND hivatal.meddig <= 1700
+
+/*6*/
+SELECT uralkodo.nev, 
+    (hivatal.meddig - hivatal.mettol) + 1
+FROM kiralyok.uralkodo
+    INNER JOIN kiralyok.hivatal ON hivatal.uralkodo_az = uralkodo.azon
+ORDER BY 2 DESC
+LIMIT 1
+
+/*7*/
 SELECT
-  uralkodo.nev
+  uralkodo.nev,
+  hivatal.mettol - uralkodo.szul AS 'Uralkodási Év'
 FROM kiralyok.uralkodo
   INNER JOIN kiralyok.hivatal
     ON hivatal.uralkodo_az = uralkodo.azon
-WHERE hivatal.koronazas < hivatal.mettol
-
-
-/*5*/
-
-
-
-/*6*/
-
-
-
-/*7*/
-
-
+HAVING `Uralkodási Év` < 15
+ORDER BY 2 
 
 /*8*/
 
 
-
 /*9*/
+
