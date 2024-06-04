7d68e9389aea8cdd68b67133eb5794e2992ba40a
Szszabi <67242037+szszabi2002@users.noreply.github.com>
2024-06-04 08:35:39 +0200

    Állóképesség kész

--- Adatbázis/Szincsák Szabolcs_ujABK/Megoldások/Szincsák Szabolcs_allokep.sql
+++ Adatbázis/Szincsák Szabolcs_ujABK/Megoldások/Szincsák Szabolcs_allokep.sql
@@ -1,73 +1,72 @@
 -- Active: 1706598324977@@127.0.0.1@3306@allokep
 
 /*2*/
-SELECT
-  megye.letszam
-FROM megye
-WHERE megye.nev LIKE "%Vas%"
+SELECT megye.letszam FROM megye WHERE megye.nev LIKE "%Vas%"
 
 /*3*/
-SELECT
-  SUM(aerob.letszam)
+SELECT SUM(aerob.letszam)
 FROM aerob
-  INNER JOIN megye
-    ON aerob.mkod = megye.kod
-WHERE megye.nev LIKE "%Somogy%" 
+    INNER JOIN megye ON aerob.mkod = megye.kod
+WHERE
+    megye.nev LIKE "%Somogy%"
 
 /*4*/
-SELECT
-  aerob.letszam
+SELECT aerob.letszam
 FROM aerob
-  INNER JOIN allapot
-    ON aerob.allkod = allapot.kod
-  INNER JOIN megye
-    ON aerob.mkod = megye.kod
-WHERE megye.nev LIKE "%Zala%"
-AND aerob.nem = 1
-AND allapot.nev LIKE "%egészséges%"
+    INNER JOIN allapot ON aerob.allkod = allapot.kod
+    INNER JOIN megye ON aerob.mkod = megye.kod
+WHERE
+    megye.nev LIKE "%Zala%"
+    AND aerob.nem = 1
+    AND allapot.nev LIKE "%egészséges%"
 
 /*5*/
-SELECT
-    COUNT(*) AS megyek_szama
-FROM
-    megye
+SELECT COUNT(*) AS megyek_szama
+FROM megye
 WHERE
     letszam < (
-        SELECT
-            letszam
-        FROM
-            megye
+        SELECT letszam
+        FROM megye
         WHERE
             nev = 'Heves'
     );
 
 /*6*/
-SELECT
-    (COUNT(aerob.azon) * 100.0 / megye.letszam) AS reszveteli_arany
-FROM
-    megye
-    LEFT JOIN aerob ON megye.kod = aerob.mkod
+SELECT COUNT(aerob.azon) * 100.0 / megye.letszam AS reszveteli_arany
+FROM megye
+    LEFT OUTER JOIN aerob ON megye.kod = aerob.mkod
 WHERE
-    megye.nev = 'Pest';
+    megye.nev LIKE "Pest"
 
 /*7*/
-SELECT
-    megye.nev,
-    COUNT(*) AS egeszseges_lanyok_szama
-FROM
-    megye
-    JOIN aerob ON megye.kod = aerob.mkod
-    JOIN allapot ON aerob.allkod = allapot.kod
+SELECT megye.nev, SUM(aerob.letszam)
+FROM megye
+    INNER JOIN aerob ON megye.kod = aerob.mkod
+    INNER JOIN allapot ON aerob.allkod = allapot.kod
 WHERE
-    allapot.nev LIKE "%egészséges%"
+    allapot.nev = 'egészséges'
     AND aerob.nem = '0'
 GROUP BY
     megye.nev
-ORDER BY
-    egeszseges_lanyok_szama DESC;
-
+ORDER BY 2 DESC
 
 /*8*/
-
+SELECT COUNT(aerob.azon) * 100.0 / megye.letszam AS reszveteli_arany
+FROM megye
+    LEFT OUTER JOIN aerob ON megye.kod = aerob.mkod
+GROUP BY
+    megye.nev
+ORDER BY 1 DESC
+LIMIT 1
 
 /*9*/
+SELECT megye.nev AS Megyenév, SUM(aerob.letszam) / megye.letszam AS Arány
+FROM aerob
+    INNER JOIN megye ON aerob.mkod = megye.kod
+    INNER JOIN allapot ON aerob.allkod = allapot.kod
+WHERE
+    allapot.nev NOT LIKE 'egészséges'
+GROUP BY
+    megye.nev
+HAVING
+    Arány > 0.25
